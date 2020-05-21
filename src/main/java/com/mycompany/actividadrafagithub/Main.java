de0763260fc3d6c1382c082ac786c9cbd4e3219f
VICTOR <VICTOR@DESKTOP-B7D4EN6.lan>
2020-05-21 14:39:26 +0200

    Primera prueba

--- src/main/java/com/mycompany/actividadrafagithub/Main.java
+++ src/main/java/com/mycompany/actividadrafagithub/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
          System.out.println("Hola2");
+                 System.out.println("Hola");
+         System.out.println("Hola2");
+
+                 System.out.println("Hola");
+         System.out.println("Hola2");
+
+
+                 
     }
 }
