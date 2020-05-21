33b30db2a8e8a4dc4f7e4d9b0dadc20745cd36fa
VICTOR <VICTOR@DESKTOP-B7D4EN6.lan>
2020-05-21 14:40:58 +0200

    Cuarta prueba

--- src/main/java/com/mycompany/actividadrafagithub/Main.java
+++ src/main/java/com/mycompany/actividadrafagithub/Main.java
@@ -3,12 +3,10 @@ package com.mycompany.actividadrafagithub;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
-         System.out.println("Hola2");
-                 System.out.println("Hola");
-         System.out.println("Hola2");
-
-                 System.out.println("Hola");
-         System.out.println("Hola2");
+        System.out.println("Hola2");
+        System.out.println("Hola");
+        System.out.println("Hola2");
+        System.out.println("Hola");
 
 
                  
